ce85fd56ce94a52e6c2ad0098642100e5d3cc65f
cg33 <chg80333@163.com>
2022-05-10 10:39:52 +0800

    init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module go-terminal
+
+go 1.16
+
+require golang.org/x/term v0.0.0-20220411215600-e5f449aeb171
---
+++ terminal.go
@@ -0,0 +1,50 @@
+package terminal
+
+import (
+	"golang.org/x/term"
+	"io"
+	"os"
+)
+
+type shell struct {
+	r io.Reader
+	w io.Writer
+}
+
+func (sh *shell) Read(data []byte) (n int, err error) {
+	return sh.r.Read(data)
+}
+
+func (sh *shell) Write(data []byte) (n int, err error) {
+	return sh.w.Write(data)
+}
+
+type Terminal struct {
+	oldState *term.State
+	term     *term.Terminal
+	fd       int
+}
+
+func NewTerminal(prompt string) (*Terminal, error) {
+	sh := &shell{r: os.Stdin, w: os.Stdout}
+	te := term.NewTerminal(sh, prompt)
+	fd := int(os.Stdin.Fd())
+	oldState, err := term.MakeRaw(fd)
+	return &Terminal{
+		oldState: oldState,
+		term:     te,
+		fd:       fd,
+	}, err
+}
+
+func (t *Terminal) ReadLine() (line string, err error) {
+	return t.term.ReadLine()
+}
+
+func (t *Terminal) Write(b []byte) (n int, err error) {
+	return t.term.Write(b)
+}
+
+func (t *Terminal) Close() {
+	term.Restore(t.fd, t.oldState)
+}
